75f2406a8c14cd1d7c623d782a7b434e8ff116f7
dborra03 <davide.borra03@gmail.com>
2023-01-02 22:06:25 +0100

    No more hardcoded arrays - or at least they are in a different file

---
+++ appunti/notes.js
@@ -0,0 +1,36 @@
+notes = [
+        	{
+                title: "Geometria A - mod 1",
+                date: "29/11/22",
+                text: "Appunti del corso di Geometria A - mod 1 tenuto dal prof. L.E. Sola Conde",
+                link: "https://davideborra.notion.site/Geometria-A-f587dd8f407f40cf8c3097028891a791",
+                linkText: "Geometria A",
+                topic: "Geometria",
+                school: "Uni",
+                showDetails: false,
+                show: true,
+            },
+            {
+                title: "Analisi Matematica A - mod 1",
+                date: "9/12/22",
+                text: "Trascrizioni delle esercitazioni di Analisi tenute da S. Vercellesi",
+                link: "https://github.com/dborra03/esercitazione-analisi-A-2022-23",
+                linkText: "Analisi A - GitHub",
+                topic: "Analisi",
+                school: "Uni",
+                showDetails: false,
+                show: true,
+            },
+            {
+                title: "Funzioni ℝ → ℝ",
+                date: "26/12/22",
+                text: "Riassunto prodotto da me.",
+                link: "https://github.com/davideborra/appunti-liceo/raw/main/funzioni/main.pdf",
+                linkText: "Funzioni",
+                topic: "Analisi",
+                school: "Liceo",
+                showDetails: false,
+                show: true,
+            },
+            
+        ];
--- appunti/script.js
+++ appunti/script.js
@@ -21,44 +21,10 @@ var app = new Vue({
                 pageSrc: "../informatica"
             }
         ],
-        notes: [
-        	{
-                title: "Geometria A - mod 1",
-                date: "29/11/22",
-                text: "Appunti del corso di Geometria A - mod 1 tenuto dal prof. L.E. Sola Conde",
-                link: "https://davideborra.notion.site/Geometria-A-f587dd8f407f40cf8c3097028891a791",
-                linkText: "Geometria A",
-                topic: "Geometria",
-                school: "Uni",
-                showDetails: false,
-                show: true,
-            },
-            {
-                title: "Analisi Matematica A - mod 1",
-                date: "9/12/22",
-                text: "Trascrizioni delle esercitazioni di Analisi tenute da S. Vercellesi",
-                link: "https://github.com/dborra03/esercitazione-analisi-A-2022-23",
-                linkText: "Analisi A - GitHub",
-                topic: "Analisi",
-                school: "Uni",
-                showDetails: false,
-                show: true,
-            },
-            {
-                title: "Funzioni ℝ → ℝ",
-                date: "26/12/22",
-                text: "Riassunto prodotto da me.",
-                link: "https://github.com/davideborra/appunti-liceo/raw/main/funzioni/main.pdf",
-                linkText: "Funzioni",
-                topic: "Analisi",
-                school: "Liceo",
-                showDetails: false,
-                show: true,
-            },
-            
-        ]
+        notes: []
     },
     mounted(){
+        this.notes = notes
         for (note of this.notes){
             if (!this.topics.includes(note.topic)){
                 this.topics.push(note.topic);   //append
