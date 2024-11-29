4d274c04b2d354f57fa81d90f6cc64dbb929a4cc
Davide Borra <davide.borra03@gmail.com>
2024-11-29 17:13:50 +0100

    patch note divisibilità-arit-mod

--- appunti/galilei/notes.js
+++ appunti/galilei/notes.js
@@ -5,11 +5,11 @@ notes = [
                 text: "Introduzione ad alcuni strumenti dell'Algebra Lineare, applicazione dell'algoritmo di diagonalizzazione per la determinazione della formula generale per l'n-esimo termine della successione di Fibonacci.",
                 link: [
                     {
-                        link: "notes/linalg_fib/alg-lineare-fibonacci-note.pdf",
+                        link: "notes/alg-lineare-fibonacci-note.pdf",
                         text: "Note in LaTeX"
                     },
                     {
-                        link: "notes/linalg_fib/alg-lineare-fibonacci-in-aula.pdf",
+                        link: "notes/alg-lineare-fibonacci-in-aula.pdf",
                         text: "Lavagna"
                     }
                 ],
@@ -23,7 +23,7 @@ notes = [
                 text: "Introduzione alla teoria degli anelli commutativi con unità, strutturata attraverso l'esempio dell'anello dei numeri interi. Divisione con resto, MCD, MCM, anelli ℤ/nℤ, teorema cinese del resto.",
                 link: [
                     {
-                        link: "notes/div-arit-mod/Divisibilita-aritmetica-modulare.pdf",
+                        link: "notes/Divisibilita-aritmetica-modulare.pdf",
                         text: "Note in LaTeX"
                     }
                 ],
